e7e02ad370e548ab00672686ad6072bb5967a8ca
Gina <gina.agapescu@gmail.com>
2020-10-18 06:29:20 +0300

    Create project

---
+++ src/main/java/com/gina/expressMenu/ExpressMenuApplication.java
@@ -0,0 +1,13 @@
+package com.gina.expressMenu;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExpressMenuApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ExpressMenuApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/gina/expressMenu/ExpressMenuApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gina.expressMenu;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExpressMenuApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
